e81f2e2c9f7c038cfa427b54b649b4f489540927
superfrank <frank.hulley-jones@guardian.co.uk>
2020-07-03 18:40:58 +0100

    Added mustache and doc

--- atoms/default/server/render.js
+++ atoms/default/server/render.js
@@ -1,5 +1,28 @@
-import mainHTML from "./atoms/default/server/templates/main.html!text"
+
+import mainHTML from "./atoms/default/server/templates/main.html!text";
+import share from "./atoms/default/server/templates/share.html!text";
+import rp from "request-promise";
+import { writeFileSync } from "fs";
+import Mustache from "mustache";
+
+const partials = { share };
 
 export async function render() {
-    return mainHTML;
-} 
+  const html = await rp(
+    "https://interactive.guim.co.uk/docsdata-test/1c6XFRYUS4KoRmtLXb6rO-mpjaBLQvxqqCx-xh4KgAsw.json"
+  );
+  let htmlJson = JSON.parse(html);
+  writeFileSync("./assets/copy.json", htmlJson);
+  // writeFileSync('./assets/related.json', related)
+
+  Object.keys(htmlJson).map((key) => {
+    if (key.indexOf("copy") > -1) {
+      htmlJson[key] = htmlJson[key].replace(/[\r\n]+/g, "\n").split("\n");
+    }
+  });
+
+  // console.log(htmlJson)
+
+  const copy = Mustache.render(mainHTML, htmlJson, partials);
+  return copy;
+}
