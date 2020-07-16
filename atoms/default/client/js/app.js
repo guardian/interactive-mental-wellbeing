ac3e9eb5e9dcb2e7563bfed2ffb66f13877e46ae
Garry Blight <garry_blight@33249.gnm.int>
2020-07-16 12:23:00 +0100

    Added navigation

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,2 +1,16 @@
-// if you want to import a module from shared/js then you can
-// just do e.g. import Scatter from "shared/js/scatter.js"
+import { $, $$, wait } from "shared/js/util";
+
+
+
+$$(".gv-nav").forEach((el) => {
+    
+    el.addEventListener("click", function(e){
+        const target = e.target;
+        const anchor = target.getAttribute("data-anchor");
+        const jumpto = $(".section-" + anchor);
+        const yOffset = -10;
+        const y = jumpto.getBoundingClientRect().top + window.pageYOffset + yOffset;
+        window.scrollTo({top: y, behavior: 'smooth'});
+    });
+  });
+
